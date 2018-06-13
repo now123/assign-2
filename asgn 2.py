c76bb386735d9796894ec60660f97639a0d3204d
now123 <smithtarun999@gmail.com>
2018-06-13 21:25:46 +0530

    asgn 2

---
+++ asgn 2.py
@@ -0,0 +1,25 @@
+# QUE1-
+print ("i am the best")
+
+#QUE2-
+print ("acad"  + "view")
+
+#QUE3-
+x = 5
+y = 6
+z = 7
+print(x,y,z)
+
+#QUE4-
+print ('let\'s get started')
+
+#QUE5-
+s = "acadview"
+course = "python"
+fees = 500
+print (' institue = %s course is = %s and fees = %d' %(s , course , fees))
+
+#QUE6-
+name = "tony stark"
+salary =  1000000
+print ('%s %d'% (name,salary))
